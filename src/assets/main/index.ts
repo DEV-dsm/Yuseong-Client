24501f42d8d8b0a93a866235267f7db584ba575f
서유정 <seoyujeong1234@gmail.com>
2023-12-04 09:28:04 +0900

    📷 :: svg 파일/코드 추가

---
+++ src/assets/main/index.ts
@@ -0,0 +1,11 @@
+import calendarIcon from './calendarIcon.svg';
+import arrowUnderIcon from './arrowUnderIcon.svg';
+import resetIcon from './resetIcon.svg';
+import searchIcon from './searchIcon.svg';
+
+export const MainImages = {
+    calendarIcon,
+    arrowUnderIcon,
+    resetIcon,
+    searchIcon,
+};
