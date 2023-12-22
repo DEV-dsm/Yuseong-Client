3bd24e19e480c5aaa8e02248efe90592c7131029
서유정 <seoyujeong1234@gmail.com>
2023-12-22 09:11:35 +0900

    📷 :: svg 파일/코드 추가

--- src/assets/main/index.ts
+++ src/assets/main/index.ts
@@ -2,10 +2,24 @@ import calendarIcon from './calendarIcon.svg';
 import arrowUnderIcon from './arrowUnderIcon.svg';
 import resetIcon from './resetIcon.svg';
 import searchIcon from './searchIcon.svg';
+import default_selectIcon from './default_selectIcon.svg';
+import default_modifyIcon from './default_modifyIcon.svg';
+import default_viewIcon from './default_viewIcon.svg';
+import click_selectIcon from './click_selectIcon.svg';
+import click_modifyIcon from './click_modifyIcon.svg';
+import click_viewIcon from './click_viewIcon.svg';
+import checkBoxIcon from './checkBoxIcon.svg';
 
 export const MainImages = {
     calendarIcon,
     arrowUnderIcon,
     resetIcon,
     searchIcon,
+    default_selectIcon,
+    default_modifyIcon,
+    default_viewIcon,
+    click_selectIcon,
+    click_modifyIcon,
+    click_viewIcon,
+    checkBoxIcon,
 };
